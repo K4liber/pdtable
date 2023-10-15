9c7a1baa1820bd34ac717d15db1e2592a80609ae
K4liber <janbielecki94@gmail.com>
2023-10-15 11:11:28 +0200

    improvement

--- pdtable/specific.py
+++ pdtable/specific.py
@@ -1,9 +1,11 @@
-from abc import ABC, abstractstaticmethod
+from abc import ABC
 from dataclasses import dataclass
+
+from pdtable.frame import TableDataFrame
 from .proxy import Table
 
 
-class ValidationException(Exception):
+class SpecificationException(Exception):
     pass
 
 
@@ -15,41 +17,88 @@ class ColumnDetails:
 
 class SpecificDataTable(ABC, Table):
 
-    _NAME = ''
-    _DESTINATIONS = {'all'}
+    _NAME: str | None = None
+    _DESTINATIONS: set[str] = {'all'}
+    _COLUMNS: dict[str, ColumnDetails] | None = None
 
-    @abstractstaticmethod
-    def column_name_to_details() -> dict[str, ColumnDetails]:
-        ...
-    
+    def __init__(self, table_data_frame: TableDataFrame, validate_all: bool = False) -> None:
+        if self._COLUMNS is None:
+            raise SpecificationException(
+                f'Specific data table needs to define a class attribute "_COLUMNS".'
+            )
+        
+        super().__init__(df=table_data_frame)
+
+        if validate_all:
+            self.validate()
+
+    @staticmethod
+    def _camel_to_snake(s: str) -> str:
+        return ''.join(['_' + c.lower() if c.isupper() else c for c in s]).lstrip('_')
+
+    @classmethod
+    def _specified_name(cls) -> str:
+        return cls._NAME if cls._NAME else cls._camel_to_snake(cls.__name__)
+
     @classmethod
     def get_description(cls) -> list[str]:
         description_lines = []
-        description_lines.append(f'Table "{cls._NAME}"\n')
-        description_lines.append(f'Columns:\n')
+        description_lines.append(f'Table "{cls._specified_name()}"\n')
+        destinations_str = str(cls._DESTINATIONS).replace("'", '"')
+        description_lines.append(f'Destinations: {destinations_str}\n')
 
-        for column_name, column_details in cls.column_name_to_details().items():
-            description_lines.append(f'    {column_name} [{column_details.unit}] "{column_details.description}"\n')
+        if cls._COLUMNS is not None:
+            description_lines.append(f'Columns:\n')
 
-        description_lines.append('Description:\n')
+            for column_name, column_details in cls._COLUMNS.items():
+                description_lines.append(
+                    f'    {column_name} [{column_details.unit}] "{column_details.description}"\n')
 
-        for description_line in cls.__doc__.split('\n'):
-            stripped = description_line.strip()
+        doc = cls.__doc__
 
-            if stripped != '':
-                description_lines.append(f'    {stripped}\n')
+        if doc:
+            description_lines.append('Description:\n')
+
+            for description_line in cls.__doc__.split('\n'):
+                stripped = description_line.strip()
+
+                if stripped != '':
+                    description_lines.append(f'    {stripped}\n')
 
         return description_lines
 
-    def validate(self: 'SpecificDataTable') -> None:
-        assert self._NAME == self.name
-        column_names = set(self.column_name_to_details().keys())
+    def validate(
+            self: 'SpecificDataTable',
+            validate_name: bool = True,
+            validate_columns: bool = True,
+            validate_units: bool = True,
+            validate_destinations: bool = True
+        ) -> None:
+        specified_name = self._specified_name()
 
-        if column_names != set(self.df.columns):
-            raise ValidationException(f'Table "{self._NAME}" should have the following columns: {column_names}.')
+        if validate_name:
+            if specified_name != self.name:
+                raise SpecificationException(
+                    f'Name mismatch. Expected = "{specified_name}", got = "{self.name}".')
 
-        for index, col_name in enumerate(self.df.columns):
-            unit = self.units[index]
-            assert unit == self.column_name_to_details()[col_name].unit
+        if validate_columns:
+            column_names = set(self._COLUMNS.keys())
 
-        assert self._DESTINATIONS == self.destinations
+            if column_names != set(self.df.columns):
+                raise SpecificationException(
+                    f'Table "{specified_name}" expected to have the following columns: {column_names}.')
+
+            if validate_units:
+                for index, col_name in enumerate(self.df.columns):
+                    unit = self.units[index]
+                    specified_unit = self._COLUMNS[col_name].unit
+
+                    if unit != specified_unit:
+                        raise SpecificationException(
+                            f'Unit mismatch for column "{col_name}". '
+                            f'Expected = [{self._COLUMNS[col_name].unit}], got = [{unit}].')
+
+        if validate_destinations:
+            if self._DESTINATIONS != self.destinations:
+                raise SpecificationException(
+                    f'Destinations mismatch. Expected = {self._DESTINATIONS}, got = {self.destinations}.')
--- pdtable/test/test_specific.py
+++ pdtable/test/test_specific.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import pandas as pd
 from pytest import fixture
-from pdtable.frame import TableDataFrame
 from pdtable.specific import SpecificDataTable, ColumnDetails
 from pdtable.proxy import Table
 
@@ -11,30 +10,21 @@ class Animals(SpecificDataTable):
     Table store the data about all animals in the world.
     The purpose is to select the best animals for Noah's ark.
     """
-    _NAME = 'animals'
 
-    def __init__(self, table_data_frame: TableDataFrame, validate: bool = False) -> None:
-        super().__init__(df=table_data_frame)
-
-        if validate:
-            self.validate()
-
-    @staticmethod
-    def column_name_to_details() -> dict[str, ColumnDetails]:
-        return {
-            'name': ColumnDetails(
-                unit='text',
-                description='Name of the animal'
-            ),
-            'weight': ColumnDetails(
-                unit='kg',
-                description='Weight of the animal (as it has on the birth day)'
-            ),
-            'number_of_legs': ColumnDetails(
-                unit='-',
-                description='Number of legs of the animal'
-            )
-        }
+    _COLUMNS = {
+        'name': ColumnDetails(
+            unit='text',
+            description='Name of the animal'
+        ),
+        'weight': ColumnDetails(
+            unit='kg',
+            description='Weight of the animal (as it has on the birth day)'
+        ),
+        'number_of_legs': ColumnDetails(
+            unit='-',
+            description='Number of legs of the animal'
+        )
+    }
 
 
 @fixture
@@ -54,8 +44,7 @@ def test_animals_data_table(test_resources: Path):
         name="animals",
         units=["text", "kg", "-"],
     )
-    animals = Animals(table_data_frame=table.df)
-    animals.validate()
+    animals = Animals(table_data_frame=table.df, validate_all=True)
     actual_description_lines = animals.get_description()
 
     with open(test_resources / 'animals.txt', 'r') as file:
